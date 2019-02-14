53e50de32006f829a84ba6efa2021bd7c58df832
DevExpressExampleBot <fakeEmail>
2019-02-15 01:13:22 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication3/Form1.Designer.vb
+++ VB/WindowsApplication3/Form1.Designer.vb
@@ -1,137 +1,133 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace DXSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace DXSample
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colCustomerID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colFirstName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colLastName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colAge = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.repositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
-			Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemPictureEdit1})
-			Me.gridControl1.Size = New System.Drawing.Size(503, 337)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Appearance.HeaderPanel.Options.UseTextOptions = True
-			Me.gridView1.Appearance.HeaderPanel.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCustomerID, Me.colFirstName, Me.colLastName, Me.colAge})
-			Me.gridView1.CustomizationFormBounds = New System.Drawing.Rectangle(818, 81, 218, 255)
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.OptionsView.ColumnAutoWidth = False
-			' 
-			' colCustomerID
-			' 
-			Me.colCustomerID.Caption = "Long Long Long Caption for CustomerID"
-			Me.colCustomerID.FieldName = "CustomerID"
-			Me.colCustomerID.Name = "colCustomerID"
-			Me.colCustomerID.Visible = True
-			Me.colCustomerID.VisibleIndex = 0
-			Me.colCustomerID.Width = 103
-			' 
-			' colFirstName
-			' 
-			Me.colFirstName.Caption = "Long Caption for FirstName"
-			Me.colFirstName.FieldName = "FirstName"
-			Me.colFirstName.Name = "colFirstName"
-			Me.colFirstName.Visible = True
-			Me.colFirstName.VisibleIndex = 1
-			Me.colFirstName.Width = 130
-			' 
-			' colLastName
-			' 
-			Me.colLastName.Caption = "Long Long Long Long Long Caption for LastName"
-			Me.colLastName.FieldName = "LastName"
-			Me.colLastName.Name = "colLastName"
-			Me.colLastName.Visible = True
-			Me.colLastName.VisibleIndex = 2
-			Me.colLastName.Width = 150
-			' 
-			' colAge
-			' 
-			Me.colAge.Caption = "Long Caption for Age"
-			Me.colAge.FieldName = "Age"
-			Me.colAge.Name = "colAge"
-			Me.colAge.Visible = True
-			Me.colAge.VisibleIndex = 3
-			Me.colAge.Width = 133
-			' 
-			' repositoryItemPictureEdit1
-			' 
-			Me.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1"
-			' 
-			' defaultLookAndFeel1
-			' 
-			Me.defaultLookAndFeel1.LookAndFeel.SkinName = "Blue"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(503, 337)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-			Me.Name = "Form1"
-			Me.Text = "ColumnPanel Auto Height"
-'			Me.FormClosing += New System.Windows.Forms.FormClosingEventHandler(Me.OnFormClosing);
-'			Me.Load += New System.EventHandler(Me.OnFormLoad);
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colCustomerID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colFirstName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colLastName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colAge = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.repositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+            Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemPictureEdit1})
+            Me.gridControl1.Size = New System.Drawing.Size(503, 337)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Appearance.HeaderPanel.Options.UseTextOptions = True
+            Me.gridView1.Appearance.HeaderPanel.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCustomerID, Me.colFirstName, Me.colLastName, Me.colAge})
+            Me.gridView1.CustomizationFormBounds = New System.Drawing.Rectangle(818, 81, 218, 255)
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsView.ColumnAutoWidth = False
+            ' 
+            ' colCustomerID
+            ' 
+            Me.colCustomerID.Caption = "Long Long Long Caption for CustomerID"
+            Me.colCustomerID.FieldName = "CustomerID"
+            Me.colCustomerID.Name = "colCustomerID"
+            Me.colCustomerID.Visible = True
+            Me.colCustomerID.VisibleIndex = 0
+            Me.colCustomerID.Width = 103
+            ' 
+            ' colFirstName
+            ' 
+            Me.colFirstName.Caption = "Long Caption for FirstName"
+            Me.colFirstName.FieldName = "FirstName"
+            Me.colFirstName.Name = "colFirstName"
+            Me.colFirstName.Visible = True
+            Me.colFirstName.VisibleIndex = 1
+            Me.colFirstName.Width = 130
+            ' 
+            ' colLastName
+            ' 
+            Me.colLastName.Caption = "Long Long Long Long Long Caption for LastName"
+            Me.colLastName.FieldName = "LastName"
+            Me.colLastName.Name = "colLastName"
+            Me.colLastName.Visible = True
+            Me.colLastName.VisibleIndex = 2
+            Me.colLastName.Width = 150
+            ' 
+            ' colAge
+            ' 
+            Me.colAge.Caption = "Long Caption for Age"
+            Me.colAge.FieldName = "Age"
+            Me.colAge.Name = "colAge"
+            Me.colAge.Visible = True
+            Me.colAge.VisibleIndex = 3
+            Me.colAge.Width = 133
+            ' 
+            ' repositoryItemPictureEdit1
+            ' 
+            Me.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1"
+            ' 
+            ' defaultLookAndFeel1
+            ' 
+            Me.defaultLookAndFeel1.LookAndFeel.SkinName = "Blue"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(503, 337)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+            Me.Name = "Form1"
+            Me.Text = "ColumnPanel Auto Height"
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private repositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
-		Private colCustomerID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colFirstName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colLastName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colAge As DevExpress.XtraGrid.Columns.GridColumn
-		Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
-	End Class
+        #End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private repositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+        Private colCustomerID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colFirstName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colLastName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colAge As DevExpress.XtraGrid.Columns.GridColumn
+        Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    End Class
 End Namespace
 
